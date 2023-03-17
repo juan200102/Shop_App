92b47e4d89ce5ba2ef0ead51e2bf2fe8e8818062
JH <juanfelipeduque318@gmail.com>
2023-03-17 11:52:23 -0500

    aplicacion flutter coffe

---
+++ coffe/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:coffe/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(239, 10, 10, 10)
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
